ca80a515eb43bbd1cc7404991e4317831c2f021f
Torbary <torbarey020@gmail.com>
2023-05-26 22:42:50 +0100

    Updated api/v1/app.py to create a CORS instance allowing: /* for 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""app.py"""
+"""Flask API app"""
 
+from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_views
-import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
-
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Closes the database session"""
-    storage.close()
+# Create a CORS instance and allow all origins for now
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Handler for 404 errors"""
+    """Error handler for 404 not found"""
     return jsonify({"error": "Not found"}), 404
 
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Close the storage"""
+    storage.close()
 
 if __name__ == "__main__":
     """run the Flask with"""
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
-
-
