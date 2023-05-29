bfcde63fa27b85db23959cf95c6a9db7f814810b
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-29 15:32:19 +0100

    Add stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,3 +15,17 @@ def show_status():
     shows the status
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def count_classes():
+    """
+    Counts the numbers of objects owned by a class
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")
+                    })
