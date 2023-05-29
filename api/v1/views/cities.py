3791e3d2e909f84dbf9a483992ccc24d285a266d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-29 23:50:15 +0100

    Added a new method to city to retrieve city objets using the city id

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -26,3 +26,17 @@ def retrieve_city_using_stateid(state_id):
             cities_list.append(city.to_dict())
         return jsonify(cities_list)
     abort(404)
+
+
+@app_views.route("/cities/<city_id>", methods=["GET"],
+                 strict_slashes=False)
+def retrieve_city_using_cityid(city_id):
+    """
+    Retrieves a city using the city id
+    returns a 404 error if the id doesn't match any city
+    """
+
+    city = storage.get(City, city_id)
+    if city:
+        return jsonify(city.to_dict())
+    abort(404)
